ee054c49793bcc7093718b131fcca822b21e270a
devanshichavda <46798129+devanshichavda@users.noreply.github.com>
2025-03-06 20:14:45 +0000

    added login() and main() functions login() function: takes user input for username   and password. Checks password with hash in the   logins text file and output successful login   message if correct. Outputs incorrect login   message if incorrect. Asks user to register if   the username is not stored in the text file. main() function:   Allows user to choose between logging in,   registering, and exiting. Calls login function   or register function depending on input.

--- main.py
+++ main.py
@@ -33,5 +33,42 @@ def register():
     return
 
   password_hash = create_hash(password)
+  save_new_user(username, password_hash)
+  print("You're now registered!")
 
+def login():
+  #enter username and password and check if values correspond
+  print("Login")
+  username = input("Enter username: ")
+  password = input("Enter password: ")
 
+  stored_hash = get_stored_info(username)
+  if stored_hash:
+    if create_hash(password) == stored_hash:
+      print('Login Successful.')
+      return True
+    else:
+      print("Incorrect Password.")
+      return False
+  else:
+    print("Username not found. Please register.")
+    return False
+
+def main():
+  while True:
+    print("\n1. Register")
+    print("2. Login")
+    print("3. Exit")
+
+    choice = input("Enter your choice (1-3):")
+
+    if choice == '1':
+      register()
+    elif choice == '2':
+      login()
+    elif choice == '3':
+      print("Bye!")
+      break
+    else:
+      print('Invalid.')
+main()
